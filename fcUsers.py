269a206508650fc038dcb9f8d4098494b8f024e7
ErickModesto <erick.c.modesto@gmail.com>
2020-06-16 18:15:46 -0300

    Acredito que j aestá funcionando

--- fcUsers.py
+++ fcUsers.py
@@ -14,14 +14,18 @@ def pred(user,item,k):
         sumU = 0
         sumV = 0
         result = 0
+        uBias = 0
+        vBias = 0
         if i == user or math.isnan(mat[i][item]):
             continue
         uBias = np.nanmean(mat[user,:])#user test
         vBias = np.nanmean(mat[i,:])#user train
         if math.isnan(uBias):
-            uBias = 0
+            #print('uBias is NaN')
+            return
         if math.isnan(vBias):
-            vBias = 0
+            #print('vBias is NaN')
+            continue
         for j in range(0,3564):
             if math.isnan(mat[user][j]) or math.isnan(mat[i][j]):
                 continue
@@ -29,16 +33,19 @@ def pred(user,item,k):
                 u.append(mat[user][j])#user test
                 v.append(mat[i][j])#user train
         if len(u) == 0:
-            sim.append((0,i))
+            continue
         for x in range(0, len(u)):
             sum1 += (u[x]-uBias) * (v[x]-vBias)
             sumU += np.power((u[x]-uBias),2)
             sumV += np.power((v[x]-vBias),2)
+            sum1 = round(sum1,4) 
+            sumU = round(sumU,4) 
+            sumV = round(sumV,4) 
         result = sum1/(np.sqrt(sumU) * np.sqrt(sumV))#similarity
+        result = round(result,4)
         if math.isnan(result):
             continue
         else:
-            #print(result)
             sim.append((result,i))
         u.clear()
         v.clear()
@@ -46,12 +53,21 @@ def pred(user,item,k):
     sum1 = 0
     sum2 = 0
     pred = 0
+    if k > len(sim):
+        k = len(sim)
     for y in range(0,k):
         vUser = sim[y][1] 
         sum1 += sim[y][0] * (mat[vUser][item] - vBias)
         sum2 += sim[y][0]
-    pred = uBias + sum1/sum2
+        sum1 = round(sum1,4)
+        sum2 = round(sum2,4)
     sim.clear()
+    if sum2 == 0:
+        print('Erro', end=' ')
+        print(sum2)
+        return
+    pred = uBias + sum1/sum2
+    pred = round(pred,4)
     print(pred)
 
 if __name__=='__main__':
