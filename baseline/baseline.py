7af4d7bdd271d802562e0f0fe201dc24b99b7f2e
ErickModesto <erick.c.modesto@gmail.com>
2020-06-16 18:16:01 -0300

    Método Baseline

---
+++ baseline/baseline.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import math
+
+def baseline():
+    mat = np.load('../train.npy')
+    iBias = np.apply_along_axis(average, 0, mat)#0:col; 1:lin
+    uBias = np.apply_along_axis(average, 1, mat)#0:col; 1:lin
+    np.save('movieBaseline.npy',iBias) 
+    np.save('userBaseline.npy',uBias) 
+
+
+def average(index):
+    mat = np.load('../train.npy')
+    globalMean = np.nanmean(mat)
+    total = 0
+    cont = 0
+    for i in index:
+        if math.isnan(i):
+            continue
+        else:
+            total += i-globalMean
+            cont += 1
+    if cont == 0:
+        mean = np.NaN
+    else:
+        mean = total/cont
+    return mean
+
+if __name__=='__main__':
+    baseline()     
---
+++ baseline/pred.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+def pred(user,item,mean):
+    movieBaseline = np.load('movieBaseline.npy')
+    userBaseline = np.load('userBaseline.npy')
+    result = mean + movieBaseline[item] + userBaseline[user]
+    result = round(result,4)
+    print(result)
+
+if __name__=='__main__':
+    test = pd.read_csv('../data/test.csv')
+    mat = np.load('../train.npy')
+    mean = np.nanmean(mat)
+    nada = []
+    for ind in test.index:
+        user = test['user_id'][ind]-1
+        item = test['movie_id'][ind]-1
+        pred(user,item,mean)
