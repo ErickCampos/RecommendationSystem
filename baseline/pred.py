bc36212507e0cb30f8e41ff3f8b62f44b49837cd
Erick <erick.modesto@gmail.com>
2020-06-20 11:37:02 -0300

    Lista não utilizada foi removida

--- baseline/pred.py
+++ baseline/pred.py
@@ -5,14 +5,13 @@ def pred(user,item,mean):
     movieBaseline = np.load('movieBaseline.npy')
     userBaseline = np.load('userBaseline.npy')
     result = mean + movieBaseline[item] + userBaseline[user]
-    result = round(result,4)
+    result = round(result,2)
     print(result)
 
 if __name__=='__main__':
     test = pd.read_csv('../data/test.csv')
     mat = np.load('../train.npy')
     mean = np.nanmean(mat)
-    nada = []
     for ind in test.index:
         user = test['user_id'][ind]-1
         item = test['movie_id'][ind]-1
