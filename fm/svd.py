b50725ac4f9f11432ab022a798dfdc25aec21f67
Erick <erick.modesto@gmail.com>
2020-06-20 18:51:38 -0300

    entrada de K pelo terminal

--- fm/svd.py
+++ fm/svd.py
@@ -3,19 +3,7 @@ import pandas as pd
 import math 
 from scipy.sparse.linalg import svds
 import time
-
-def replaceNaN():
-    mat = np.load('../train.npy')
-    for i in range(0,3974):
-        mean = np.nanmean(mat[i,:])
-        for j in range(0,3564):
-            if math.isnan(mat[i][j]):
-                if math.isnan(mean):
-                    mean = np.nanmean(mat[:,j])
-                if math.isnan(mean):
-                    mean = 0
-                mat[i][j] = mean
-    return mat
+import sys
 
 def pred(u,sigma,v,user,item):
     p = u[user,:]
@@ -29,12 +17,20 @@ def pred(u,sigma,v,user,item):
     print(rating)
 
 if __name__=='__main__':
-    #start_time = time.time()
+    k = sys.argv[1]
     test = pd.read_csv('../data/test.csv')
-    mat = replaceNaN()
-    u, s, v = svds(mat, k=5)
+    mat = np.load('meanTrain.npy')
+    start_time = time.time()
+    u, s, v = svds(mat,int(k))
+    print('id,rating')
+    cont = 0
     for ind in test.index:                                                      
         user = test['user_id'][ind]-1                                           
         item = test['movie_id'][ind]-1                                          
+        print(cont,end=',')
         pred(u,s,v.transpose(),user,item)
-    #print(time.time() - start_time)
+        cont += 1
+    arquivo = open('NaN.txt','a') # Abra o arquivo (leitura)
+    arquivo.write(str(time.time() - start_time))
+    arquivo.write('\n')
+    arquivo.close()
